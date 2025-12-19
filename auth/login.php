9d38511a9900cbbc1ac2d2c216a31eb2eca613c3
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2025-12-19 01:02:57 +0100

    setup verifying

--- auth/login.php
+++ auth/login.php
@@ -3,35 +3,34 @@
 require_once __DIR__ . "/../database.php";
 session_start();
 
-try{
+try {
 
 
-    $email=$_POST["email"];
+    $email = $_POST["email"];
 
-    $password=$_POST["password"];
+    $password = $_POST["password"];
 
-   $stmt= $db->prepare("select * from users where email=?;");
-   $stmt->execute([$email]);
+    $stmt = $db->prepare("select * from users where email=?;");
+    $stmt->execute([$email]);
 
-   if($stmt->rowCount() === 1){
+    if ($stmt->rowCount() === 1) {
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        if(password_verify($password, $user["password"])){
+        if (password_verify($password, $user["password"])) {
             $_SESSION["user_id"] = $user["id"];
-            header("location: /index.php");
+            header("location: /verify.php");
             exit();
-        }else{
+        } else {
             $_SESSION["error"] = "Invalid";
             header("location: /login.php");
             exit();
         }
-   }else{
+    } else {
         $_SESSION["error"] = "Invalid";
         header("location: /login.php");
         exit();
-   }
+    }
 
-}
-catch( PDOException $e){
+} catch (PDOException $e) {
     echo $e->getmessage();
 }
 
---
+++ verify.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <script src="https://cdn.tailwindcss.com"></script>
+    <title>login</title>
+</head>
+
+<body class="bg-gray-200 flex items-center justify-center ">
+
+  <form  action=""name="is_verified" class="bg-white w-800 h-auto p-6 rounded-lg shadow-md flex flex-col gap- items-center flex flex-cols gap-8 justify-evenly">
+      
+      <label class="text-lg font-semibold">
+        Check your inbox
+      </label>
+<input 
+        class="border-2 border-gray-700 h-12 w-200 px-3 rounded"
+        placeholder="Enter your code">
+<button class="bg-gray-800 hover:bg-gray-600 h-20 w-60">confirm</button>
+</form> 
+
+</body>
+</html>
